a8fcdee79a4565fe9395d41859a063deaa961918
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-05 15:37:16 -0300

    feat: configure global settings for PostgreSQL connection and add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -7,7 +11,30 @@ import { ProductModule } from './product/product.module';
 import { PackageModule } from './package/package.module';
 
 @Module({
-  imports: [UserModule, ScheduleModule, ProductModule, PackageModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+
+    UserModule,
+    ScheduleModule,
+    ProductModule,
+    PackageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
