1a61b4c920ac970957744db9ddd57a20cf90b489
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-05 15:37:58 -0300

    feat: enhance User entity with constructor and getter/setter methods for properties

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -23,4 +23,72 @@ export class User {
 
   @Column({ type: 'enum', enum: Role })
   role: Role;
+
+  constructor(
+    name: string,
+    cpf: string,
+    email: string,
+    phone: string,
+    password: string,
+    role: Role,
+  ) {
+    this.name = name;
+    this.cpf = cpf;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+    this.role = role;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public setCpf(cpf: string): void {
+    this.cpf = cpf;
+  }
+
+  public setEmail(email: string): void {
+    this.email = email;
+  }
+
+  public setPhone(phone: string): void {
+    this.phone = phone;
+  }
+
+  public setPassword(password: string): void {
+    this.password = password;
+  }
+
+  public setRole(role: Role): void {
+    this.role = role;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getCpf(): string {
+    return this.cpf;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public getPhone(): string {
+    return this.phone;
+  }
+
+  public getPassword(): string {
+    return this.password;
+  }
+
+  public getRole(): Role {
+    return this.role;
+  }
 }
