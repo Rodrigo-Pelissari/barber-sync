87bcc4526ff5c8d1512fee84fe409670f368dba9
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-05 15:37:27 -0300

    feat: create CreatePackageDto and update Package entity with necessary properties and methods

--- src/package/dto/create-package.dto.ts
+++ src/package/dto/create-package.dto.ts
@@ -1,1 +1,28 @@
-export class CreatePackageDto {}
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsInt,
+  IsNumber,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import { Schedule } from 'src/schedule/entities/schedule.entity';
+import { User } from 'src/user/entities/user.entity';
+
+export class CreatePackageDto {
+  @IsObject()
+  @ValidateNested()
+  @Type(() => User)
+  customer: User;
+
+  @IsNumber()
+  value: number;
+
+  @IsInt()
+  servicesQuantity: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Schedule)
+  usedServices: Schedule[];
+}
--- src/package/entities/package.entity.ts
+++ src/package/entities/package.entity.ts
@@ -13,6 +13,57 @@ export class Package {
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   value: number;
 
+  @Column({ type: 'int' })
+  servicesQuantity: number;
+
   @Column()
   usedServices: Schedule[];
+
+  constructor(
+    customer: User,
+    value: number,
+    servicesQuantity: number,
+    usedServices: Schedule[],
+  ) {
+    this.customer = customer;
+    this.value = value;
+    this.servicesQuantity = servicesQuantity;
+    this.usedServices = usedServices;
+  }
+
+  public setCustomer(customer: User): void {
+    this.customer = customer;
+  }
+
+  public setValue(value: number): void {
+    this.value = value;
+  }
+
+  public setServicesQuantity(servicesQuantity: number): void {
+    this.servicesQuantity = servicesQuantity;
+  }
+
+  public setUsedServices(usedServices: Schedule[]): void {
+    this.usedServices = usedServices;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getCustomer(): User {
+    return this.customer;
+  }
+
+  public getValue(): number {
+    return this.value;
+  }
+
+  public getServicesQuantity(): number {
+    return this.servicesQuantity;
+  }
+
+  public getUsedServices(): Schedule[] {
+    return this.usedServices;
+  }
 }
