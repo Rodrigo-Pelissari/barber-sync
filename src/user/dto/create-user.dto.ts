76d5b515213346992d23a1ef2d229eee9ae06823
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-05 15:37:54 -0300

    feat: implement CreateUserDto with validation and role properties

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsString } from 'class-validator';
+import { Role } from '../enums/role.enum';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  cpf: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  password: string;
+
+  role: Role;
+}
