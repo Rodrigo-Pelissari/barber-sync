d2e23cf4fcfc7d1f8c2f6aa2a107b21454b99fd2
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-10 11:48:47 -0300

    feat: implement ScheduleService methods for schedule management and retrieval

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,28 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import { Schedule } from './entities/schedule.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ScheduleRepository } from './schedule.repository';
+import { ScheduleDto } from './dto/schedule.dto';
 
 @Injectable()
 export class ScheduleService {
-  create(createScheduleDto: CreateScheduleDto) {
-    return 'This action adds a new schedule';
+  constructor(
+    @InjectRepository(Schedule)
+    private readonly scheduleRepository: ScheduleRepository,
+  ) {}
+
+  public async create(
+    createScheduleDto: CreateScheduleDto,
+  ): Promise<ScheduleDto> {
+    const entity = createScheduleDto.toEntity();
+
+    this.setScheduleValue(entity);
+
+    await this.scheduleRepository.save(entity);
+
+    return new ScheduleDto(
+      entity.id,
+      entity.barber,
+      entity.customer,
+      entity.date,
+      entity.type,
+      entity.service,
+      entity.value,
+    );
   }
 
-  findAll() {
-    return `This action returns all schedule`;
+  public async findAll(): Promise<ScheduleDto[]> {
+    const schedules = await this.scheduleRepository.findAll();
+
+    return schedules.map(
+      (schedule) =>
+        new ScheduleDto(
+          schedule.id,
+          schedule.barber,
+          schedule.customer,
+          schedule.date,
+          schedule.type,
+          schedule.service,
+          schedule.value,
+        ),
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} schedule`;
+  public async findById(id: string): Promise<ScheduleDto | null> {
+    const schedule = await this.scheduleRepository.findById(id);
+
+    if (!schedule)
+      throw new NotFoundException(`Schedule with id ${id} not found`);
+
+    return new ScheduleDto(
+      schedule.id,
+      schedule.barber,
+      schedule.customer,
+      schedule.date,
+      schedule.type,
+      schedule.service,
+      schedule.value,
+    );
   }
 
-  update(id: number, updateScheduleDto: UpdateScheduleDto) {
-    return `This action updates a #${id} schedule`;
+  public async update(
+    id: string,
+    updateScheduleDto: UpdateScheduleDto,
+  ): Promise<ScheduleDto | null> {
+    const schedule = await this.scheduleRepository.findById(id);
+
+    if (!schedule)
+      throw new NotFoundException(`Schedule with id ${id} not found`);
+
+    updateScheduleDto.update(schedule);
+
+    const updatedSchedule = await this.scheduleRepository.save(schedule);
+
+    return new ScheduleDto(
+      updatedSchedule.id,
+      updatedSchedule.barber,
+      updatedSchedule.customer,
+      updatedSchedule.date,
+      updatedSchedule.type,
+      updatedSchedule.service,
+      updatedSchedule.value,
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} schedule`;
+  public async delete(id: string): Promise<void> {
+    await this.scheduleRepository.delete(id);
   }
 
   public setScheduleValue(schedule: Schedule): void {
