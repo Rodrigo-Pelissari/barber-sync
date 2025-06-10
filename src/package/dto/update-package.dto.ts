e450dc6635d238c848e2f6796de68f4bcfdbe4f5
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-10 10:55:27 -0300

    feat: add update methods to UpdatePackageDto, UpdateScheduleDto, and UpdateUserDto for entity modification

--- src/package/dto/update-package.dto.ts
+++ src/package/dto/update-package.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePackageDto } from './create-package.dto';
+import { Package } from '../entities/package.entity';
 
-export class UpdatePackageDto extends PartialType(CreatePackageDto) {}
+export class UpdatePackageDto extends PartialType(CreatePackageDto) {
+  public update(entity: Package): void {
+    if (this.customer !== undefined) entity.customer = this.customer;
+    if (this.value !== undefined) entity.value = this.value;
+    if (this.servicesQuantity !== undefined)
+      entity.servicesQuantity = this.servicesQuantity;
+    if (this.usedServices !== undefined)
+      entity.usedServices = this.usedServices;
+  }
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { User } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  public update(entity: User): void {
+    if (this.name !== undefined) entity.name = this.name;
+    if (this.cpf !== undefined) entity.cpf = this.cpf;
+    if (this.email !== undefined) entity.email = this.email;
+    if (this.phone !== undefined) entity.phone = this.phone;
+    if (this.role !== undefined) entity.role = this.role;
+  }
+}
