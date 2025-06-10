2bc43f39731e09b9b47c616bde2b64222b5f217a
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-10 11:48:34 -0300

    feat: implement CRUD methods in PackageService for package management

--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -1,26 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Package } from './entities/package.entity';
+import { PackageDto } from './dto/package.dto';
+import { PackageRepository } from './package.repository';
 
 @Injectable()
 export class PackageService {
-  create(createPackageDto: CreatePackageDto) {
-    return 'This action adds a new package';
+  constructor(
+    @InjectRepository(Package)
+    private readonly repository: PackageRepository,
+  ) {}
+
+  public async create(createPackageDto: CreatePackageDto): Promise<PackageDto> {
+    const entity = createPackageDto.toEntity();
+    const savedEntity = await this.repository.save(entity);
+
+    return new PackageDto(
+      savedEntity.id,
+      savedEntity.customer,
+      savedEntity.value,
+      savedEntity.servicesQuantity,
+      savedEntity.usedServices,
+    );
   }
 
-  findAll() {
-    return `This action returns all package`;
+  public async findAll(): Promise<PackageDto[]> {
+    const entities = await this.repository.findAll();
+
+    return entities.map(
+      (entity) =>
+        new PackageDto(
+          entity.id,
+          entity.customer,
+          entity.value,
+          entity.servicesQuantity,
+          entity.usedServices,
+        ),
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} package`;
+  public async findById(id: string): Promise<PackageDto> {
+    const entity = await this.repository.findById(id);
+    if (!entity) {
+      throw new Error(`Package with id ${id} not found`);
+    }
+    return new PackageDto(
+      entity.id,
+      entity.customer,
+      entity.value,
+      entity.servicesQuantity,
+      entity.usedServices,
+    );
   }
 
-  update(id: number, updatePackageDto: UpdatePackageDto) {
-    return `This action updates a #${id} package`;
+  public async update(
+    id: string,
+    updatePackageDto: UpdatePackageDto,
+  ): Promise<PackageDto> {
+    const entity = await this.repository.findById(id);
+
+    if (!entity) throw new NotFoundException(`Package with id ${id} not found`);
+
+    updatePackageDto.update(entity);
+
+    const updatedEntity = await this.repository.save(entity);
+
+    return new PackageDto(
+      updatedEntity.id,
+      updatedEntity.customer,
+      updatedEntity.value,
+      updatedEntity.servicesQuantity,
+      updatedEntity.usedServices,
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} package`;
+  public async remove(id: string): Promise<void> {
+    await this.repository.delete(id);
   }
 }
