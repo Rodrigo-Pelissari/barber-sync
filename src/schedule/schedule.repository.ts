6c5c97edbca56f44861e6483164ae0a4ef08112f
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-12 11:29:39 -0300

    refactor: update method visibility to public in ScheduleRepository

--- src/schedule/schedule.repository.ts
+++ src/schedule/schedule.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Schedule } from './entities/schedule.entity';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class ScheduleRepository {
@@ -10,19 +10,47 @@ export class ScheduleRepository {
     private readonly repository: Repository<Schedule>,
   ) {}
 
-  async save(entity: Schedule): Promise<Schedule> {
+  public async save(entity: Schedule): Promise<Schedule> {
     return await this.repository.save(entity);
   }
 
-  async findAll(): Promise<Schedule[]> {
+  public async findAll(): Promise<Schedule[]> {
     return await this.repository.find();
   }
 
-  async findById(id: string): Promise<Schedule | null> {
+  public async findById(id: string): Promise<Schedule | null> {
     return await this.repository.findOneBy({ id });
   }
 
-  async delete(id: string): Promise<void> {
+  public async delete(id: string): Promise<void> {
     await this.repository.delete(id);
+  }
+
+  public async findByUser(userId: string): Promise<Schedule[]> {
+    return await this.repository.find({
+      where: [
+        { barber: { id: userId } } as FindOptionsWhere<Schedule>,
+        { customer: { id: userId } } as FindOptionsWhere<Schedule>,
+      ],
+      relations: ['barber', 'customer', 'services'],
+    });
+  }
+
+  public async findConcludedSchedulesByUser(
+    userId: string,
+  ): Promise<Schedule[]> {
+    return await this.repository.find({
+      where: [
+        {
+          barber: { id: userId },
+          concluded: true,
+        } as FindOptionsWhere<Schedule>,
+        {
+          customer: { id: userId },
+          concluded: true,
+        } as FindOptionsWhere<Schedule>,
+      ],
+      relations: ['barber', 'customer', 'services'],
+    });
   }
 }
