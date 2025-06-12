e14f6ce6cc0b55961fda39119ef095888ba0f875
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-12 14:24:37 -0300

    refactor: remove update method from UpdateScheduleDto for cleaner code

--- src/schedule/dto/update-schedule.dto.ts
+++ src/schedule/dto/update-schedule.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateScheduleDto } from './create-schedule.dto';
-import { Schedule } from '../entities/schedule.entity';
 
-export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {
-  public update(entity: Schedule): void {
-    if (this.date !== undefined) entity.date = this.date;
-    if (this.type !== undefined) entity.type = this.type;
-  }
-}
+export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {}
