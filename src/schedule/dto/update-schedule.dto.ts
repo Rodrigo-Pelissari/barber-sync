fec28325901c2d38527eb7889b4b754a632697be
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-12 14:19:55 -0300

    refactor: remove unused properties from UpdateScheduleDto for cleaner update logic

--- src/schedule/dto/update-schedule.dto.ts
+++ src/schedule/dto/update-schedule.dto.ts
@@ -4,11 +4,7 @@ import { Schedule } from '../entities/schedule.entity';
 
 export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {
   public update(entity: Schedule): void {
-    if (this.customer !== undefined) entity.customer = this.customer;
     if (this.date !== undefined) entity.date = this.date;
     if (this.type !== undefined) entity.type = this.type;
-    if (this.service !== undefined) entity.service = this.service;
-    if (this.value !== undefined) entity.value = this.value;
-    if (this.barber !== undefined) entity.barber = this.barber;
   }
 }
