05d9aea41cad8e42024a679ccdcdad0a328ccf2f
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-12 14:12:44 -0300

    refactor: simplify CreateScheduleDto by removing unused properties and methods

--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,47 +1,11 @@
-import { Product } from 'src/product/entities/product.entity';
-import { User } from 'src/user/entities/user.entity';
 import { serviceType } from '../enums/serviceType.enum';
-import { IsNumber, IsObject, IsString, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
-import { Schedule } from '../entities/schedule.entity';
+import { IsString } from 'class-validator';
 
 export class CreateScheduleDto {
-  @IsObject()
-  @ValidateNested()
-  @Type(() => User)
-  barber: User;
-
-  @IsObject()
-  @ValidateNested()
-  @Type(() => User)
-  customer: User;
-
-  @IsObject()
-  @ValidateNested()
-  @Type(() => Date)
-  date: Date;
-
   type: serviceType;
 
-  @IsObject()
-  @ValidateNested()
-  @Type(() => Product)
-  service: Product[];
-
-  @IsNumber()
-  value: number;
+  date: Date;
 
   @IsString({ each: true })
   productsNames: string[];
-
-  public toEntity(): Schedule {
-    return new Schedule(
-      this.barber,
-      this.customer,
-      this.date,
-      this.type,
-      this.service,
-      this.value,
-    );
-  }
 }
