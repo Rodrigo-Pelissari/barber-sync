659bda18001a69d13169fc7c932cd97585f8200f
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-12 12:28:05 -0300

    refactor: update ComissionModule to include TypeOrmModule and ComissionRepository

--- src/comission/comission.module.ts
+++ src/comission/comission.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ComissionService } from './comission.service';
 import { ComissionController } from './comission.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comission } from './entities/comission.entity';
+import { ComissionRepository } from './comission.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comission])],
   controllers: [ComissionController],
-  providers: [ComissionService],
+  providers: [ComissionService, ComissionRepository],
+  exports: [ComissionService, ComissionRepository],
 })
 export class ComissionModule {}
