00acae5b28cb59442d58e079d10c9cddf4a6c73d
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-13 12:21:04 -0300

    feat: add ProductModule to PackageModule imports for enhanced functionality

--- src/package/package.module.ts
+++ src/package/package.module.ts
@@ -4,9 +4,10 @@ import { PackageController } from './package.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Package } from './entities/package.entity';
 import { PackageRepository } from './package.repository';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Package])],
+  imports: [TypeOrmModule.forFeature([Package]), ProductModule],
   controllers: [PackageController],
   providers: [PackageService, PackageRepository],
   exports: [PackageService, PackageRepository],
