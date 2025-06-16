afa2b578c81d8d03e6d7aadddcb96497afe1578e
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-16 12:21:23 -0300

    feat: add PackageProductRepository to PackageModule providers and exports

--- src/package/package.module.ts
+++ src/package/package.module.ts
@@ -6,11 +6,12 @@ import { Package } from './entities/package.entity';
 import { PackageRepository } from './package.repository';
 import { ProductModule } from 'src/product/product.module';
 import { UserModule } from 'src/user/user.module';
+import { PackageProductRepository } from './package-product.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Package]), ProductModule, UserModule],
   controllers: [PackageController],
-  providers: [PackageService, PackageRepository],
-  exports: [PackageService, PackageRepository],
+  providers: [PackageService, PackageRepository, PackageProductRepository],
+  exports: [PackageService, PackageRepository, PackageProductRepository],
 })
 export class PackageModule {}
