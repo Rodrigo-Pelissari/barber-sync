efa69b830ce1b1a3857e297a5300b97d3c2a60b9
Rodrigo Pelissari <rodrigod.pelissari@gmail.com>
2025-06-16 12:20:16 -0300

    feat: add UserModule to PackageModule imports for user management

--- src/package/package.module.ts
+++ src/package/package.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Package } from './entities/package.entity';
 import { PackageRepository } from './package.repository';
 import { ProductModule } from 'src/product/product.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Package]), ProductModule],
+  imports: [TypeOrmModule.forFeature([Package]), ProductModule, UserModule],
   controllers: [PackageController],
   providers: [PackageService, PackageRepository],
   exports: [PackageService, PackageRepository],
